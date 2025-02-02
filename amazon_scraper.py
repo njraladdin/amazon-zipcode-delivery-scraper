dd61f4942695de6af52c5e925a6fcb2cef85082b
unknown <aladynjr@gmail.com>
2025-02-02 01:43:23 +0100

    use normal requests instead of tls_client

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -9,7 +9,7 @@ Amazon Scraper Request Flow:
 7. Repeat all offers page request with Prime-only filter (if Prime filter available)
 """
 
-import tls_client
+import requests
 from bs4 import BeautifulSoup
 import json
 import http.cookies
@@ -44,12 +44,13 @@ class AmazonScraper:
         # Format the proxy string
         proxy = f"http://{username}:{password}@{ip}:{port}"
         
-        self.session = tls_client.Session(
-            client_identifier="chrome126",
-            random_tls_extension_order=True
-        )
-        # Set proxy directly on the session
-        self.session.proxies = proxy
+        # Replace tls_client session with requests session
+        self.session = requests.Session()
+        # Set proxy for requests session
+        self.session.proxies = {
+            'http': proxy,
+            'https': proxy
+        }
         
         # Only create output directory if saving is enabled
         self.output_dir = 'output'
